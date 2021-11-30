aa698873b927718ddbab112f5f0f2d5a5d5af32b
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2021-11-30 00:12:46 -0300

    att menu mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 // VARIÁVEIS
+// const BTN_TOPO = document.getElementById("btn-topo");
+// const TOGGLE_MENU = document.querySelectorAll('.beb-toggle-menu');
+// const MENU_MOBILE = document.querySelector('.beb-menu-mobile');
+// const BTN_MENU_MOB_ICON = document.querySelector('.beb-btn-menu-mobile i');
+
 const BTN_TOPO = document.getElementById("btn-topo");
-const TOGGLE_MENU = document.querySelectorAll('.beb-toggle-menu');
+const TOGGLE_MENU = document.querySelectorAll('.icon');
 const MENU_MOBILE = document.querySelector('.beb-menu-mobile');
-const BTN_MENU_MOB_ICON = document.querySelector('.beb-btn-menu-mobile i');
+const OPEN_ANIM_BTN = document.getElementById('menu-anim-btn');
 
 // QUANDO O USUÁRIO ROLAR PARA BAIXO DE 250PX DO TOPO O SITE, MOSTRA O BOTÃO VOLTAR PARA TOPO.
 window.onscroll = function() {scrollFunction()};
@@ -22,20 +27,14 @@ function voltarTopo() {
     document.documentElement.scrollTop = 0;
 }
 
+
+
 //Abrindo e Fechando O MENU MOBILE
 for (let m = 0; m < TOGGLE_MENU.length; m++) {
     TOGGLE_MENU[m].addEventListener('click', function () {
+        OPEN_ANIM_BTN.classList.toggle('open');
         MENU_MOBILE.classList.toggle('beb-menu-is-closed');
         MENU_MOBILE.classList.toggle('beb-menu-is-open');
-
-        var icon = BTN_MENU_MOB_ICON.getAttribute('class');
-
-        if (icon === 'fas fa-bars') {
-            BTN_MENU_MOB_ICON.setAttribute('class', 'fas fa-times');
-        } else {
-            BTN_MENU_MOB_ICON.setAttribute('class', 'fas fa-bars');
-        }
-
     });
 }
 
