55a20c4cfbcb9e3fa5d51d09e66c6ae536962893
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2021-11-30 09:27:03 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
 // VARIÁVEIS
-// const BTN_TOPO = document.getElementById("btn-topo");
-// const TOGGLE_MENU = document.querySelectorAll('.beb-toggle-menu');
-// const MENU_MOBILE = document.querySelector('.beb-menu-mobile');
-// const BTN_MENU_MOB_ICON = document.querySelector('.beb-btn-menu-mobile i');
-
 const BTN_TOPO = document.getElementById("btn-topo");
 const TOGGLE_MENU = document.querySelectorAll('.icon');
 const MENU_MOBILE = document.querySelector('.beb-menu-mobile');
@@ -11,7 +6,6 @@ const OPEN_ANIM_BTN = document.getElementById('menu-anim-btn');
 
 // QUANDO O USUÁRIO ROLAR PARA BAIXO DE 250PX DO TOPO O SITE, MOSTRA O BOTÃO VOLTAR PARA TOPO.
 window.onscroll = function() {scrollFunction()};
-
 
 function scrollFunction() {
     if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
@@ -26,8 +20,6 @@ function voltarTopo() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-
 
 //Abrindo e Fechando O MENU MOBILE
 for (let m = 0; m < TOGGLE_MENU.length; m++) {
